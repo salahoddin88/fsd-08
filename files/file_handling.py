0633905ea83995d4ed84ecfcf135f1107e2e6f25
Salahuddin Shaikh <33448178+salahoddin88@users.noreply.github.com>
2021-12-04 20:11:09 +0530

    File Handling

---
+++ files/file_handling.py
@@ -0,0 +1,37 @@
+"""
+
+    r = read
+    w = write(overwrite)
+    a = append (append on exsiting)
+    r+ = read and write(append)
+
+"""
+
+# fileObject = open('workfile.txt', 'r')
+# readData = fileObject.readline()
+# fileObject.close()
+# print(fileObject.closed)
+
+
+# This operation will read your data from existing file
+# with open('workfile.txt', 'r') as readObject:
+#     readData = readObject.read()
+#     print(readData)
+
+
+# # this operation will opverwrite your data on existing file
+# with open('workfile.txt', 'w') as writeObject:
+#     writeObject.write('My name is suresh')
+
+
+# This operation will append your data on existing file content
+# with open('workfile.txt', 'a') as appendObject:
+#     appendObject.write(' I am python developer')
+
+
+with open('workfile.txt', 'r+') as fileObject:
+    readData = fileObject.read()
+    print(readData)
+    fileObject.seek(0)  # reposition the pointer at 0th position
+    fileObject.truncate(0)
+    fileObject.write('Hello')
